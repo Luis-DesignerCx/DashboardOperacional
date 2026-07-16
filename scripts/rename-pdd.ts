cd962cf4d90eec37ea6924b8d77465683161b4d6
Luis-DesignerCx <1304lgsouza@gmail.com>
2026-07-16 12:50:29 -0300

    chore: remover scripts temporarios e arquivos de migracao ja executados

--- scripts/rename-pdd.ts
+++
@@ -1,7 +0,0 @@
-import { PrismaClient } from "@prisma/client";
-const prisma = new PrismaClient();
-async function main() {
-  const e = await prisma.equipe.update({ where: { id: "eq-91-180" }, data: { nome: "PDD 91+" } });
-  console.log("Renomeado para:", e.nome);
-}
-main().catch(console.error).finally(() => prisma.$disconnect());
